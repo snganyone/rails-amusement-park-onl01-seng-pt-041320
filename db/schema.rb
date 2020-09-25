46cf7637d7a1110945a20d4ca1831ac0f9b31b21
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-24 21:09:33 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_25_033616) do
+ActiveRecord::Schema.define(version: 2020_09_25_040722) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_09_25_033616) do
     t.integer "happiness"
     t.integer "tickets"
     t.integer "height"
+    t.boolean "admin", default: false
+    t.string "password_digest"
   end
 
 end
