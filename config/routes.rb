af8bbd97d8dbfa41c9118186ba8eac185b621919
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-25 20:54:41 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'application#home'
+
+  post '/users/new' => 'users#create'
+
+  resources :users, only: [:new, :create, :show, :edit, :update]
 end
