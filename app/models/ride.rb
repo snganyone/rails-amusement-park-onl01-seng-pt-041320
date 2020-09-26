3d998d6aab1caa3b47e06ab0b4a3d0a01fed603d
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-25 17:15:48 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,8 +1,16 @@
+require 'pry'
 class Ride < ActiveRecord::Base
     belongs_to :user
     belongs_to :attraction
 
     def take_ride
-        
+        #binding.pry
+        if self.user.tickets < self.attraction.tickets
+            return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+        elsif self.user.height < self.attraction.min_height
+            return "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+        elsif self.user.height < self.attraction.min_height and self.user.tickets < self.attraction.tickets
+            return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+        end
     end
 end 
